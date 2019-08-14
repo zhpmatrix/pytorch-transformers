7dc445761ab4b3b7f99ea23d94ed804b300cd1b5
zhanghaipeng <zhanghaipeng@aidigger.com>
2019-08-14 20:47:15 +0800

    construct new example

--- examples/utils_event.py
+++ examples/utils_event.py
@@ -1,4 +1,5 @@
 import json
+import itertools
 from collections import Counter
 from tqdm import tqdm
 
@@ -21,7 +22,7 @@ def get_target_fields(annotation):
 
 def get_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',data_path='raw_data.json',save_path = 'data_with_label_position.json'):
     """
-        extract data from raw labeling data.
+        从标注数据中提取待处理的数据
     """
     writer = open(data_dir+save_path, 'a')
     with open(data_dir+data_path, 'r') as reader:
@@ -53,7 +54,7 @@ def get_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',da
                         
 def construct_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',data_path='data_with_label_position.json',save_path = 'data.json'):
     """
-        construct train/dev data.
+        数据构建
     """
     writer = open(data_dir+save_path, 'a')
     with open(data_dir+data_path, 'r') as reader:
@@ -61,7 +62,78 @@ def construct_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/even
         total_num = len(textlines)
         for i,line in enumerate(textlines):
             event_list = json.loads(line)['event_list']
+            text_dict = Counter([event['text'] for event in event_list])
+            for text, cnt in text_dict.items():
+                if cnt > 1:# 相同段落中有多个事件
+                    round_bucket, money_bucket, subject_bucket, org_invest_bucket = get_buckets(text, event_list)
+                    examples = get_examples(round_bucket, money_bucket, subject_bucket, org_invest_bucket, text, event_list)
+                                    
     writer.close()
+
+def get_examples(round_bucket, money_bucket, subject_bucket, org_invest_bucket, text, event_list):
+    ground_truth = []
+    for event in event_list:
+        if event['text'] == text:
+            ground_truth.append(event)
+   
+    print('输入文本: ', text)
+    print('\n')
+
+    print('真实事件:')
+    for ground in ground_truth:
+        anno_list = ground['anno_list']
+        print([anno[0]['label'] for anno in anno_list])
+        print([anno[1]['text'] for anno in anno_list])
+    print('\n') 
+
+    buckets = [round_bucket, money_bucket, subject_bucket, org_invest_bucket]
+    
+    round_list = [round_[1]['text'] for round_ in round_bucket]
+    money_list = [money[1]['text'] for money in money_bucket]
+    subject_list = [subject[1]['text'] for subject in subject_bucket]
+    org_invest_list = [org_invest[1]['text'] for org_invest in org_invest_bucket]
+    
+    print('桶:')
+    if len(round_list) > 0:
+        print(round_list)
+    if len(money_list) > 0:
+        print(money_list)
+    if len(subject_list) > 0:
+        print(subject_list)
+    if len(org_invest_list) > 0:
+        print(org_invest_list)
+    print('\n')
+
+    print('合成事件:')
+    buckets_not_none = [bucket for bucket in buckets if len(bucket) > 0]
+    feature_list = list( itertools.product(*buckets_not_none) )
+    for feature in feature_list:
+        print([f[0]['label'] for f in feature])
+        print([f[1]['text'] for f in feature])
+    print('*'*20)
+    #import pdb;pdb.set_trace()
+    return None
+
+
+def get_buckets(text, event_list):
+    target_labels = ['融资主体','投资机构','投融资金额','融资轮次']
+    round_bucket = [] 
+    money_bucket = []
+    subject_bucket = []
+    org_invest_bucket = []
+    for event in event_list:
+        if event['text'] == text:
+            anno_list = event['anno_list']
+            for anno in anno_list:
+                if anno[0]['label'] == target_labels[0]:
+                    subject_bucket.append(anno)
+                if anno[0]['label'] == target_labels[1]:
+                    org_invest_bucket.append(anno)
+                if anno[0]['label'] == target_labels[2]:
+                    money_bucket.append(anno)
+                if anno[0]['label'] == target_labels[3]:
+                    round_bucket.append(anno)
+    return round_bucket,money_bucket,subject_bucket,org_invest_bucket
 
 if __name__ == '__main__':
     #get_data()
