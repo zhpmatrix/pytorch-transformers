59410db1926548cbd6aa6665c4e1f7c79c022161
zhanghaipeng <zhanghaipeng@aidigger.com>
2019-08-14 17:40:17 +0800

    update and fix

--- examples/utils_event.py
+++ examples/utils_event.py
@@ -12,14 +12,17 @@ def get_target_fields(annotation):
     else:
         new_label = old_label[old_label.find('-')+1:]
     
-    return_anno['label'] = new_label
+    return_anno['label'] = {'label':new_label}
     return_anno['position'] = annotation['position']
     
     if new_label in target_labels:
         return_info = 1
     return return_info, return_anno
 
-def get_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',data_path='raw_data.json',save_path = 'data.json'):
+def get_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',data_path='raw_data.json',save_path = 'data_with_label_position.json'):
+    """
+        extract data from raw labeling data.
+    """
     writer = open(data_dir+save_path, 'a')
     with open(data_dir+data_path, 'r') as reader:
         textlines = reader.readlines()
@@ -27,6 +30,9 @@ def get_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',da
         for i,line in enumerate(textlines):
             print(i,total_num)
             article = json.loads(json.loads(line)['input'])
+            article_dict = {}
+            article_dict['id'] = i
+            article_dict['event_list'] = []
             for para in article:
                 if 'annotation' in para.keys():
                     annotations = para['annotation']
@@ -41,9 +47,22 @@ def get_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',da
                             info, new_anno = get_target_fields(anno)
                             if info:
                                 event_dict['anno_list'].append([new_anno['label'], new_anno['position']])
-                        writer.write(json.dumps(event_dict,ensure_ascii=False)+'\n')
+                        article_dict['event_list'].append(event_dict)
+            writer.write(json.dumps(article_dict,ensure_ascii=False)+'\n')
     writer.close()
                         
+def construct_data(data_dir = '/data/share/zhanghaipeng/data/chuangtouribao/event/',data_path='data_with_label_position.json',save_path = 'data.json'):
+    """
+        construct train/dev data.
+    """
+    writer = open(data_dir+save_path, 'a')
+    with open(data_dir+data_path, 'r') as reader:
+        textlines = reader.readlines()
+        total_num = len(textlines)
+        for i,line in enumerate(textlines):
+            event_list = json.loads(line)['event_list']
+    writer.close()
 
 if __name__ == '__main__':
-    get_data()
+    #get_data()
+    construct_data()
