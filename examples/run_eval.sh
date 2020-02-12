ca02973c21183bae3c3f95fc6fc50e4e654c24cf
zhanghaipeng <zhanghaipeng@aidigger.com>
2020-02-12 12:45:00 +0800

    add run_eval

---
+++ examples/run_eval.sh
@@ -0,0 +1,17 @@
+data_dir=/nfs/users/zhanghaipeng/data/toutiao_news/A
+output_dir=/nfs/users/zhanghaipeng/data/train/news_cls
+model_path=/nfs/users/zhanghaipeng/data/pt_bert_models/bert-base-chinese/
+ckpt=49000
+expr=0
+cuda=3
+CUDA_VISIBLE_DEVICES=$cuda python run_glue.py \
+	--model_type bert \
+	--model_name_or_path $model_path \
+	--checkpoint_path $output_path/$expr/checkpoint-$ckpt \
+	--task_name news_cls \
+	--do_eval \
+	--data_dir $data_dir \
+	--max_seq_length 30 \
+	--per_gpu_eval_batch_size 100   \
+	--overwrite_output_dir \
+	--output_dir $output_dir/$expr/
